dc3fea52b93056f6718cad99de1a0e6d04442626
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-15 17:26:10 +0530

    Palindrome & Armstrong

---
+++ Armstrong.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace projectname1 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            int n, r, Sum = 0,t;
+            Console.WriteLine("Enter The Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            t=n;
+            while (n > 0) {
+                r = n % 10;
+                Sum=Sum+(r*r*r);
+                n=n/10;        
+            
+            
+            }
+            if (t == Sum)
+            {
+                Console.WriteLine("Number is Armstrong");
+            }
+            else {
+                Console.WriteLine("Number is Not Armstrong");
+                    }
+
+
+
+
+
+
+
+
+
+
+        }
+    
+    
+    
+    }
+
+
+
+
+}
