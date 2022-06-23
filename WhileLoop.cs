d733af30e7988782207acfee168940fb769b0e39
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-23 10:55:01 +0530

    WhileLoop

---
+++ WhileLoop.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _23June
+{
+        class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x=0;
+            while (x < 5) {
+                Console.WriteLine(" ABCD ");
+                x++;
+            }
+            
+        }
+
+    }
+
+}
