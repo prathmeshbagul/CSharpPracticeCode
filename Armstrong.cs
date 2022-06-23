235387d35e12d6ec083cbd1b67b7d04ae328c2ef
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-23 10:08:35 +0530

    Updated

--- Armstrong.cs
+++ Armstrong.cs
@@ -3,42 +3,36 @@ using System;
 
 namespace projectname1 {
 
-    class Program {
+         class Program
+    {
 
-        static void Main(string[] args) {
-
-            int n, r, Sum = 0,t;
+        static void Main(string[] args)
+        {
+            IdentifyArmstrong();
+        }
+        static void IdentifyArmstrong()
+        {
+            int n, r, Sum = 0, t;
             Console.WriteLine("Enter The Number");
             n = Convert.ToInt32(Console.ReadLine());
-            t=n;
-            while (n > 0) {
+            t = n;
+            while (n > 0)
+            {
                 r = n % 10;
-                Sum=Sum+(r*r*r);
-                n=n/10;        
-            
-            
+                Sum = Sum + (r * r * r);
+                n = n / 10;
+
             }
             if (t == Sum)
             {
                 Console.WriteLine("Number is Armstrong");
             }
-            else {
+            else
+            {
                 Console.WriteLine("Number is Not Armstrong");
-                    }
-
-
-
-
-
-
-
-
-
-
+            }
         }
-    
-    
-    
+
     }
 
 
