4f95716eb13e2f1bd452fe8ed736351282f4bbfb
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-23 12:55:44 +0530

    using function

--- If-Else_If.cs
+++ If-Else_If.cs
@@ -3,37 +3,45 @@ using System;
 namespace _23June
 {
     class Program
-    {   static void Main(string[] args)
+    {
+        static void Main(string[] args)
+        {
+
+            string result = DetermineGrade();
+            Console.WriteLine(result);
+        }
+        static string DetermineGrade()
         {
             Console.WriteLine("Enter the number");
             int n = Convert.ToInt32(Console.ReadLine());
 
             if (n == 0)
             {
-                Console.WriteLine("Absent");
+                return "Absent";
             }
-            else if (n > 0 && n < 40) 
+            else if (n > 0 && n < 40)
             {
-                Console.WriteLine("Fail"); 
+                return "Fail";
             }
-            else if (n >= 40 && n < 60) 
-            { 
-                Console.WriteLine("Grade-C");
-            }
-            else if (n >= 60 && n < 80) 
-            { 
-                Console.WriteLine("Grade-B");
-            }
-            else if (n >= 80 && n <= 100) 
+            else if (n >= 40 && n < 60)
             {
-                Console.WriteLine("Grade-A");
+                return "Grade-C";
+            }
+            else if (n >= 60 && n < 80)
+            {
+                return "Grade-B";
+            }
+            else if (n >= 80 && n <= 100)
+            {
+                return "Grade-A";
             }
             else
             {
-                Console.WriteLine("Grades Exceeding The Limit");
+                return "Grades Exceeding The Limit";
             }
         }
 
-    }
 
+
+    }
 }
