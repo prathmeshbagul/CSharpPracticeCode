75485ec153ffc056fcc3d3c0a6a54cc4ccff490c
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-30 17:31:41 +0530

    Refactored

--- Alphabetpattern2.cs
+++ Alphabetpattern2.cs
@@ -1,24 +1,33 @@
 namespace June29
-{
-     class Program
+{class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the Number of rows");
-            int rows= Convert.ToInt32(Console.ReadLine());
+            int rows = Convert.ToInt32(Console.ReadLine());
             char ch = 'A';
             for (int i = 0; i <= rows; i++)
             {
-                for (int j = 0; j <= i; j++)
+                for (int j = 1; j <=i; j++)
                 {
                     Console.Write(ch);
                     ch++;
-                    
+
                 }
                 Console.WriteLine();
             }
-            
 
         }
+
+
     }
 }
+
+
+//5
+
+//A
+//BC
+//DEF
+//GHIJ
+//KLMNO
